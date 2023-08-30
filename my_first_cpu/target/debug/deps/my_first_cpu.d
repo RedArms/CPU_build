98e1a5bfd11f7864bea6992f3985ec8a5f8783e9
RedArms <reda_1030@hotmail.com>
2023-08-31 01:53:26 +0200

    First commit !

---
+++ my_first_cpu/target/debug/deps/my_first_cpu.d
@@ -0,0 +1,5 @@
+C:\Users\reda_\Documents\rust\my_first_cpu\target\debug\deps\my_first_cpu.exe: src\main.rs
+
+C:\Users\reda_\Documents\rust\my_first_cpu\target\debug\deps\my_first_cpu.d: src\main.rs
+
+src\main.rs:
---
+++ my_first_cpu/target/debug/my_first_cpu.d
@@ -0,0 +1,1 @@
+C:\Users\reda_\Documents\rust\my_first_cpu\target\debug\my_first_cpu.exe: C:\Users\reda_\Documents\rust\my_first_cpu\src\main.rs
